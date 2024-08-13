f878e90b949f76882d52c0313894d298cfd806a2
LouiGee <42655505+LouiGee@users.noreply.github.com>
2024-08-13 19:21:57 +0100

    Add files via upload

---
+++ Testing/Ratify Back End /src/main/java/com/example/Testing/TestingApplication.java
@@ -0,0 +1,29 @@
+package com.example.Testing;
+
+import com.example.Testing.Repository.RoleRepository;
+import com.example.Testing.Domain.Role;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@EnableJpaAuditing
+@EnableAsync
+@SpringBootApplication
+public class TestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestingApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner runner(RoleRepository roleRepository) {
+		return args -> {
+			if (roleRepository.findByName("USER").isEmpty()) {
+				roleRepository.save(Role.builder().name("USER").build());
+			}
+		};
+	}
+}
